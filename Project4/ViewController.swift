dc77113566141aaa6c795ab0bb2782a917017dff
cejimenez0123 <christianjimenez0123@gmail.com>
2021-03-22 11:47:51 -0400

    wkwebview

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import WebKit
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView:WKWebView!
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
